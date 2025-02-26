c604acbad615fa813931f9a7675af6891fabcaab
Dania Popov <dania@o-p-e-n.com>
2025-02-26 15:17:55 -0500

    Deleting Test

--- Package.swift
+++ Package.swift
@@ -2,24 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "stripe-kit",
-    platforms: [
-        .macOS(.v10_15)
-    ],
-    products: [
-        .library(name: "StripeKit", targets: ["StripeKit"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-    ],
-    targets: [
-        .target(name: "StripeKit", dependencies: [
-            .product(name: "AsyncHTTPClient", package: "async-http-client"),
-            .product(name: "Crypto", package: "swift-crypto"),
-        ]),
-        .testTarget(name: "StripeKitTests", dependencies: [
-            .target(name: "StripeKit")
-        ])
-    ]
+  name: "stripe-kit",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(name: "StripeKit", targets: ["StripeKit"])
+  ],
+  targets: [
+    .target(name: "StripeKit"),
+  ]
 )
